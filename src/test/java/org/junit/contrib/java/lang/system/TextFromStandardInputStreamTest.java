eae66c8c5c4df232069be89c5f7d226e9d3c0776
Stefan Birkner <mail@stefan-birkner.de>
2015-06-20 10:36:42 +0200

    Support throwing IOExceptions from System.in.
    Good code handles errors and good tests test these errors. Hence it
    must be possible to simulate that System.in throws an error. Here is
    how it is done.
    
        systemInMock.throwExceptionOnInputEnd(new IOException());

--- src/test/java/org/junit/contrib/java/lang/system/TextFromStandardInputStreamTest.java
+++ src/test/java/org/junit/contrib/java/lang/system/TextFromStandardInputStreamTest.java
@@ -5,18 +5,25 @@ import static org.hamcrest.Matchers.is;
 import static org.hamcrest.Matchers.sameInstance;
 import static org.junit.Assert.assertThat;
 import static org.junit.contrib.java.lang.system.TextFromStandardInputStream.emptyStandardInputStream;
+import static org.junit.rules.ExpectedException.none;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Scanner;
 
+import org.apache.commons.io.IOUtils;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 import org.junit.rules.Timeout;
 import org.junit.runners.model.Statement;
 
 public class TextFromStandardInputStreamTest {
 	@Rule
 	public final Timeout timeout = new Timeout(1000);
+
+	@Rule
+	public final ExpectedException thrown = none();
 
 	private final TextFromStandardInputStream systemInMock = emptyStandardInputStream();
 
@@ -88,6 +95,70 @@ public class TextFromStandardInputStreamTest {
 	}
 
 	@Test
+	public void throwsRequestedIoException() throws Throwable {
+		thrown.expect(IOException.class);
+		executeRuleWithStatement(new Statement() {
+			@Override
+			public void evaluate() throws Throwable {
+				systemInMock.throwExceptionOnInputEnd(new IOException());
+			}
+		});
+	}
+
+	@Test
+	public void providesTextBeforeRequestedIoExceptionIsThrown() {
+		thrown.expect(IOException.class);
+		executeRuleWithStatement(new Statement() {
+			@Override
+			public void evaluate() throws Throwable {
+				systemInMock.provideText("arbitrary text");
+				systemInMock.throwExceptionOnInputEnd(new IOException());
+				assertSystemInProvidesText("arbitrary text");
+				System.in.read(); //throws the exception.
+			}
+		});
+	}
+
+	@Test
+	public void providesTextAfterRequestedIoExceptionIsThrown() {
+		thrown.expect(IOException.class);
+		executeRuleWithStatement(new Statement() {
+			@Override
+			public void evaluate() throws Throwable {
+				systemInMock.provideText("arbitrary text");
+				systemInMock.throwExceptionOnInputEnd(new IOException());
+				assertSystemInProvidesText("arbitrary text");
+				System.in.read(); //throws the exception.
+			}
+		});
+	}
+
+	@Test
+	public void throwsRequestedRuntimeException() throws Throwable {
+		thrown.expect(RuntimeException.class);
+		executeRuleWithStatement(new Statement() {
+			@Override
+			public void evaluate() throws Throwable {
+				systemInMock.throwExceptionOnInputEnd(new RuntimeException());
+			}
+		});
+	}
+
+	@Test
+	public void providesTextBeforeRequestedRuntimeExceptionIsThrown() {
+		thrown.expect(RuntimeException.class);
+		executeRuleWithStatement(new Statement() {
+			@Override
+			public void evaluate() throws Throwable {
+				systemInMock.provideText("arbitrary text");
+				systemInMock.throwExceptionOnInputEnd(new RuntimeException());
+				assertSystemInProvidesText("arbitrary text");
+				System.in.read(); //throws the exception.
+			}
+		});
+	}
+
+	@Test
 	public void restoreSystemIn() throws Throwable {
 		InputStream originalSystemIn = System.in;
 		executeRuleWithStatement(new EmptyStatement());
@@ -96,5 +167,10 @@ public class TextFromStandardInputStreamTest {
 
 	private void executeRuleWithStatement(Statement statement) throws Throwable {
 		systemInMock.apply(statement, null).evaluate();
+	}
+
+	private void assertSystemInProvidesText(String text) throws IOException {
+		for (char c : text.toCharArray())
+			assertThat((char) System.in.read(), is(c));
 	}
 }
